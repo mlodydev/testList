1f14e391be034d7f66c3f02228af12ceceba2c70
Michał Wojtaszek <modyhipi@gmail.com>
2019-11-06 12:10:26 +0100

    ListItem props added

--- App.js
+++ App.js
@@ -19,27 +19,26 @@ import {
 
 import ListItem from './modules/ListItem';
 
-// const itemImageSource= 'https://statics.sportskeeda.com/editor/2018/03/a4a7b-1520474015-800.jpg';
-// const itemName = 'Michael Jordan'
-// const itemId = 23;
-// const itemUrl = 'https://pl.wikipedia.org/wiki/Michael_Jordan';
-
 class App extends Component{
   render(){
     return(
       <View>
         <FlatList
           data={[
-            {id: '1'},
-            {id: '2'},
-            {id: '3'},
-            {id: '4'},
-            {id: '5'},
-            {id: '6'},
-            {id: '7'},
-            {id: '8'},
+            {imageUrl: "https://statics.sportskeeda.com/editor/2018/03/a4a7b-1520474015-800.jpg", 
+            name: 'Michael Jordan', 
+            pageUrl: 'whatever.com', 
+            id: '1'},
+            {imageUrl: "https://statics.sportskeeda.com/editor/2018/03/a4a7b-1520474015-800.jpg", 
+            name: 'Michael Jordan', 
+            pageUrl: 'whatever.com', 
+            id: '2'},
+            {imageUrl: "https://statics.sportskeeda.com/editor/2018/03/a4a7b-1520474015-800.jpg", 
+            name: 'Michael Jordan', 
+            pageUrl: 'whatever.com', 
+            id: '3'},
           ]}
-          renderItem={({item}) => <ListItem id={item.id}/>}
+          renderItem={({item}) => <ListItem id={item.id} imageUrl={item.imageUrl} name={item.name} pageUrl={item.pageUrl}/>}
         />
       </View>
     );
