31a8a9dea0d2aaab6dd6769f939405ebd66007a4
Michał Wojtaszek <modyhipi@gmail.com>
2019-11-27 10:34:36 +0100

    redux hooks working

--- src/modules/MainScreen.js
+++ src/modules/MainScreen.js
@@ -1,49 +1,44 @@
-import React, {Component, useEffect,useState} from 'react';
+import React, { useEffect, useCallback} from 'react';
 import ListCompHooks from './ListCompHooks';
 import { useSelector, useDispatch } from 'react-redux';
 import { setFetchState, setApiData } from '../redux/reducers/listData/actions';
-import { bindActionCreators } from 'redux';  
 
 const DATA_URL = 'https://picsum.photos/v2/list';
 
-const MainScreen = (props) => {
+const MainScreen = () => {
 
   const data = useSelector(state => state.data)
   const isFetching = useSelector(state => state.isFetching)
   const dispatch = useDispatch();
 
-  
+  const setData = useCallback(
+    (data) => dispatch(setApiData(data)),
+    [dispatch]
+  )
+
+  const setFetching = useCallback(
+    (isFetching) => dispatch(setFetchState(isFetching)),
+    [dispatch]
+  )
 
   const fetchApiData = () =>{
-    dispatch(setFetchState(true));
+    setFetching(true);
     fetch(DATA_URL)
       .then(response => response.json())
         .then(responseJson => {
-          dispatch(setApiData(responseJson));
-          dispatch(setFetchState(false));
+          setData(responseJson);
+          setFetching(false);
     })
     .catch((error) => console.warn(error));
   }
 
-
+  useEffect( ()=>{
+    fetchApiData()
+  }, [])
 
   return(
       <ListCompHooks isLoading={isFetching} data={data} fetchDataMethod={fetchApiData} />
   );
 }
 
-// const mapStateToProps = (state) => {
-//   return{
-//     data: state.data,
-//     isFetching: state.isFetching
-//   }
-// }
-
-// const mapDispatchToProps = (dispatch) => {
-//   return(
-//     bindActionCreators({setApiData, setFetchState}, dispatch)
-//   );
-// }
-
-// export default connect(mapStateToProps, mapDispatchToProps)(MainScreen);
 export default MainScreen;
